27c1d033cbae0f3a210e0b1a3b22650ff0e721de
williamsimionatto <william.sn0226@gmail.com>
2024-04-09 13:55:58 -0300

    feat: finish custom sectionn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FF6363',
+        gray: {
+          ...colors.gray,
+          '900': '#181818',
+        }
+      },
+      spacing: {
+        '50': '20rem',
+      },
+    },
   },
   plugins: [],
 }
